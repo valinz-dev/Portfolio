98816fce1de961fffe5c492adac7990827e1c4f0
Valinz <149720043+valinz-dev@users.noreply.github.com>
2025-10-27 19:56:53 -0500

    Update Main.py

--- Discord-Bots/Trial-Bot/Main.py
+++ Discord-Bots/Trial-Bot/Main.py
@@ -17,7 +17,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Logged in as {bot.user}")
+    print(f" Logged in as {bot.user}")
 
 @bot.command()
 async def hello(ctx):
